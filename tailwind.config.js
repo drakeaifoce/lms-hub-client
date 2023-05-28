ab12550f28634d6480e81bb18abf24999f574a9d
drakeaifoce <drakeaifoce@gmail.com>
2023-05-28 23:19:50 +0600

    init color paletter

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,14 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+  theme:{
+    colors: {
+      current: "currentColor",
+      transparent: "transparent",
+      white: "#FFFFFF",
+      blue : "#3069FE",
+      gray: "#DEDEDE",
+      "dark-gray" : "#4D4D4D",
     },
   },
   plugins: [],
