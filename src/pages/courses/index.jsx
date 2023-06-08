93b99870cb2d68984459d70056c18f031676a13c
drakeaifoce <drakeaifoce@gmail.com>
2023-06-09 00:56:51 +0600

    Implement inital layout of courses

--- src/pages/courses/index.jsx
+++ src/pages/courses/index.jsx
@@ -1,6 +1,6 @@
 import { Layout } from "@/components/Layout"
 import Link from "next/link"
-import { Clock } from "feather-icons-react/build/IconComponents";
+import { ChevronLeft, ChevronRight, Clock } from "feather-icons-react/build/IconComponents";
 import Image from "next/image";
 import { Button } from "@/components/primitives/Button";
 
@@ -11,8 +11,9 @@ export default function AllCourses() {
           <div className="flex flex-col gap-10 md:gap-8 px-4">
               <h1 className="text-black-100 font-medium text-2xl md:text-3xl text-center">Курсы</h1>
               <div className="flex flex-col gap-6 md:gap-7">
-                <nav>
-                  Tabs
+                <nav className="flex flex-row justify-between items-center">
+                  <div>Tabs</div>
+                  <div className="md:hidden">Filters mobile</div>
                 </nav>
                 <section className="grid grid-cols-1 md:grid-cols-3 gap-5">
                   <div className="hidden col-span-1 md:block border border-solid border-black-20 rounded-lg">
@@ -38,7 +39,7 @@ export default function AllCourses() {
                         <section className="flex flex-col items-start gap-2">
                           <div className="flex flex-row text-left gap-3 items-center">
                             <h3 className="font-medium text-sm text-black">StartUp School</h3>
-                            <span className="px-1 py-1 bg-green-10 text-green-70 rounded-md font-normal text-xs">идет набор</span>
+                            <span className="px-1 py-1 bg-green-10 text-green-70 rounded-md font-normal text-xs  whitespace-nowrap">идет набор</span>
                           </div>
                           <p className="text-black-60 font-normal text-sm w-full md:w-3/4">
                           Курс по основам IT-предпринимательства для стартаперов, будущих продакт-менеджеров, школьников и студентов.
@@ -61,7 +62,7 @@ export default function AllCourses() {
                         <section className="flex flex-col items-start gap-2">
                           <div className="flex flex-row text-left gap-3 items-center">
                             <h3 className="font-medium text-sm text-black">StartUp School</h3>
-                            <span className="px-1 py-1 bg-green-10 text-green-70 rounded-md font-normal text-xs">идет набор</span>
+                            <span className="px-1 py-1 bg-green-10 text-green-70 rounded-md font-normal text-xs  whitespace-nowrap">идет набор</span>
                           </div>
                           <p className="text-black-60 font-normal text-sm w-full md:w-3/4">
                           Курс по основам IT-предпринимательства для стартаперов, будущих продакт-менеджеров, школьников и студентов.
@@ -84,7 +85,7 @@ export default function AllCourses() {
                         <section className="flex flex-col items-start gap-2">
                           <div className="flex flex-row text-left gap-3 items-center">
                             <h3 className="font-medium text-sm text-black">StartUp School</h3>
-                            <span className="px-1 py-1 bg-green-10 text-green-70 rounded-md font-normal text-xs">идет набор</span>
+                            <span className="px-1 py-1 bg-green-10 text-green-70 rounded-md font-normal text-xs whitespace-nowrap">идет набор</span>
                           </div>
                           <p className="text-black-60 font-normal text-sm w-full md:w-3/4">
                           Курс по основам IT-предпринимательства для стартаперов, будущих продакт-менеджеров, школьников и студентов.
@@ -103,11 +104,21 @@ export default function AllCourses() {
                     </Link>
                   </div>
                 </section>
-                <div className="flex flex-col md:flex-row">
-                  <span>Страница 1 из 12</span>
-                  <div className="flex flex-row gap-1">
-                    <Button variant="tertiary">Предыдущая</Button>
-                    <Button variant="tertiary">Следующая</Button>
+                <div className="flex flex-col gap-3 md:flex-row md:gap-2 md:justify-end md:items-center">
+                  <span className="text-black-60 font-normal text-sm">Страница 1 из 12</span>
+                  <div className="flex flex-row gap-1 items-center">
+                    <Button variant="tertiary" className="flex flex-row gap-1 w-full items-center text-black-60 justify-center">
+                      <ChevronLeft size={20}/>
+                      <span className="text-black-70 text-sm font-medium">
+                        Предыдущая
+                      </span>
+                    </Button>
+                    <Button variant="tertiary" className="flex flex-row gap-1 w-full items-center text-black-60 justify-center">
+                      <span className="text-black-70 text-sm font-medium">
+                        Следующая
+                      </span>
+                      <ChevronRight size={20}/>
+                    </Button>
                   </div>
                 </div>
               </div>
