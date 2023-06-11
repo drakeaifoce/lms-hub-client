4ac7057751817c03819a69f36dfea958c2035f5f
drakeaifoce <drakeaifoce@gmail.com>
2023-06-11 13:35:13 +0600

    Add some shit (profile section must be updated in design)

--- src/pages/profile/dashboard.jsx
+++ src/pages/profile/dashboard.jsx
@@ -5,7 +5,10 @@ export default function Dashboard() {
   return (
     <Layout footerHidden={true}>
       <ProfileLayout>
-        Dashboard
+        <section className="flex flex-col gap-2">
+          <h1 className="text-black-100 font-medium text-2xl">Dashboard</h1>
+          <p className="text-black-60 font-normal text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </section>
       </ProfileLayout>
     </Layout>
   )
