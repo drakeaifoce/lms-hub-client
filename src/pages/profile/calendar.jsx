f01b2335ed5422cb65ea847f8f9b56404068d23c
drakeaifoce <drakeaifoce@gmail.com>
2023-06-12 16:08:27 +0600

    Init calendar

--- src/pages/profile/calendar.jsx
+++ src/pages/profile/calendar.jsx
@@ -4,7 +4,14 @@ import { ProfileLayout } from "@/components/ProfileLayout";
 export default function Calendar() {
   return (
     <Layout footerHidden={true}>
-      <ProfileLayout>Calendar</ProfileLayout>
+      <ProfileLayout>
+        <section className="flex flex-col gap-2">
+          <h1 className="text-2xl font-medium text-black-100">Календарь</h1>
+          <p className="text-sm font-normal text-black-60">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </section>
+      </ProfileLayout>
     </Layout>
   );
 }
