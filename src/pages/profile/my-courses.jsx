7b8e558bed3da3427ebc45a301619546b4f5c0b8
drakeaifoce <drakeaifoce@gmail.com>
2023-06-14 16:49:32 +0600

    Init my courses

--- src/pages/profile/my-courses.jsx
+++ src/pages/profile/my-courses.jsx
@@ -4,7 +4,16 @@ import { ProfileLayout } from "../../components/ProfileLayout";
 export default function MyCourses() {
   return (
     <Layout footerHidden={true}>
-      <ProfileLayout>My courses</ProfileLayout>
+      <ProfileLayout>  
+        <div className="flex min-h-full flex-col">
+          <article className="flex flex-col gap-2">
+            <h1 className="text-2xl font-medium text-black-100">Мои куры</h1>
+            <p className="text-sm font-normal text-black-60">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </article>
+        </div>
+        </ProfileLayout>
     </Layout>
   );
 }
