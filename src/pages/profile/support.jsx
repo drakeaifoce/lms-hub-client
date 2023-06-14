115563f2d5109911978be2cf40ec115a58874f86
drakeaifoce <drakeaifoce@gmail.com>
2023-06-14 16:46:31 +0600

    Complete support layout

--- src/pages/profile/support.jsx
+++ src/pages/profile/support.jsx
@@ -1,10 +1,30 @@
+import { Send } from "feather-icons-react/build/IconComponents";
 import { Layout } from "../../components/Layout";
 import { ProfileLayout } from "../../components/ProfileLayout";
-
+import { TextArea } from "../../components/primitives/TextArea";
+import { Button } from "../../components/primitives/Button";
 export default function SupportService() {
   return (
     <Layout footerHidden={true}>
-      <ProfileLayout>Support</ProfileLayout>
+      <ProfileLayout> 
+        <div className="flex min-h-full flex-col gap-8">
+          <article className="flex flex-col gap-2">
+            <h1 className="text-2xl font-medium text-black-100">Мои куры</h1>
+            <p className="text-sm font-normal text-black-60">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </article>
+          <form className="flex flex-col gap-6">
+            <TextArea name="message" placeholder={"Ваше обращение"}/>
+            <Button variant="primary" className="flex flex-row justify-center items-center gap-2 w-full sm:w-fit font-medium" type="submit">
+              <Send size={14}/>
+              <span>
+                Отправить
+              </span>
+            </Button>
+          </form>
+        </div>
+        </ProfileLayout>
     </Layout>
   );
 }
