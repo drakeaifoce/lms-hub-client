67e4da435dbf552d830a7e51f5aca71b15e70b89
drakeaifoce <drakeaifoce@gmail.com>
2023-06-14 16:49:59 +0600

    Fix typography

--- src/pages/profile/my-courses.jsx
+++ src/pages/profile/my-courses.jsx
@@ -7,7 +7,7 @@ export default function MyCourses() {
       <ProfileLayout>  
         <div className="flex min-h-full flex-col">
           <article className="flex flex-col gap-2">
-            <h1 className="text-2xl font-medium text-black-100">Мои куры</h1>
+            <h1 className="text-2xl font-medium text-black-100">Мои курсы</h1>
             <p className="text-sm font-normal text-black-60">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             </p>
